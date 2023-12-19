00974e671b8a2f39d25a91c5b864f7ebab9cbb4a
luke-h1 <luke.howsam@yahoo.com>
2023-12-19 11:07:01 +0000

    feat(cli): calculate costs

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
-
 ## uber cost calculator
 
-> A simple deno script that takes in a csv file from uber's 'download your data' request and calculates how much you've spent over the lifetime of your account 🙈
+> A simple node script that takes in a csv file from uber's 'download your data' request and calculates how much you've spent over the lifetime of your account 🙈
 
 
 ### Getting started
 
 1. Request + Download your data from uber (https://myprivacy.uber.com/privacy/exploreyourdata/download)
-2. Move the csv into the root of the project
-3. Install deno (https://deno.com/)
-4. Run `deno run --allow-read --allow-write index.ts`
-5. Open the console to see the total amount spent 🙈
+2. Unzip the uber data folder and copy the `trips_data.csv` and `eats_order_details.csv` files into the 'data' folder in 'src'
+3. install dependencies with `pnpm i` - install pnpm if you don't have it already (https://pnpm.io/installation)
+4. run the script with `pnpm start`
+5. Open the console to see the total amount spent on trips and eats 🙈
 
 ### Contributing
 Feel free to contribute! I've not accounted for all the different types of data that uber can return (i.e. I've not accounted for data that includes if you've worked for Uber). If you'd like to fix up the script for your own use case, please feel free to submit a PR! 🙌
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,115 @@
-console.log("hi");
+import csvParser from "csv-parser";
+import fs from "fs";
+import path from "path";
+
+interface TripData {
+  City: string;
+  "Product Type": string;
+  "Trip or Order Status": string;
+  "Request Time": string;
+  "Begin Trip Time": string;
+  "Begin Trip Lat": string;
+  "Begin Trip Lng": string;
+  "Begin Trip Address": string;
+  "Dropoff Time": string;
+  "Dropoff Lat": string;
+  "Dropoff Lng": string;
+  "Dropoff Address": string;
+  "Distance (miles)": string;
+  "Fare Amount": string;
+  "Fare Currency": string;
+}
+
+const logTripCosts = async () => {
+  const results: TripData[] = [];
+  const __dirname = path.resolve();
+  // open file in data folder
+  const filePath = path.join(__dirname, "src/data/trips_data.csv");
+  fs.createReadStream(filePath)
+    .pipe(csvParser())
+    .on("data", (data) => {
+      results.push(data);
+    })
+    .on("end", () => {
+      const allFares: number[] = [0];
+      const endYear = results[0]["Request Time"].split(" ")[0].split("-")[0];
+      const endMonth = results[0]["Request Time"].split(" ")[0].split("-")[1];
+
+      const startYear = results[results.length - 1]["Request Time"]
+        .split(" ")[0]
+        .split("-")[0];
+      const startMonth = results[results.length - 1]["Request Time"]
+        .split(" ")[0]
+        .split("-")[1];
+
+      results.forEach((trip) => {
+        const fare = parseFloat(trip["Fare Amount"]);
+        if (fare) {
+          allFares.push(fare);
+        }
+      });
+
+      const totalCost = allFares.reduce((a, b) => a + b, 0).toFixed(2);
+      console.log(
+        `You spent £${totalCost} on uber trips between ${endMonth}/${endYear} ${startMonth}/${startYear}`
+      );
+    });
+};
+
+interface EatsOrderData {
+    Territory: string;
+    'Restaurant ID': string;
+    'Order ID': string;
+    'Order Time': string;
+    'Order Status': string;
+    'Item Name': string;
+    Customizations: string;
+    'Special Instructions': string;
+    'Item Price': string;
+    'Order Price': string;
+    Currency: string;
+}
+
+const logEatsCosts = async () => {
+    const results: EatsOrderData[] = [];
+    const __dirname = path.resolve();
+    const filePath = path.join(__dirname, "src/data/eats_order_details.csv");
+
+    fs.createReadStream(filePath)
+    .pipe(csvParser())
+    .on("data", (data) => {
+      results.push(data);
+    })
+    .on("end", () => {
+        const endYear = results[0]["Order Time"].split(" ")[0].split("-")[0];
+        const endMonth = results[0]["Order Time"].split(" ")[0].split("-")[1];
+
+        const startYear = results[results.length - 1]["Order Time"]
+          .split(" ")[0]
+          .split("-")[0];
+
+        const startMonth = results[results.length - 1]["Order Time"]
+            .split(" ")[0]
+            .split("-")[1];
+
+        const allFares: number[] = [0];
+
+        results.forEach((order) => {
+            const fare = parseFloat(order["Order Price"]);
+            if (fare) {
+              allFares.push(fare);
+            }
+          });
+
+            const totalCost = allFares.reduce((a, b) => a + b, 0).toFixed(2);
+            console.log(
+                `You spent £${totalCost} on uber eats orders between ${endMonth}/${endYear} ${startMonth}/${startYear}`
+            );
+
+
+    })
+
+}
+
+logTripCosts();
+logEatsCosts();
